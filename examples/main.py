bd10fed90ca3ac1d4ffba4afdc7ef46353fa6936
Winnie08L <105589719+Winnie08L@users.noreply.github.com>
2024-07-30 11:09:31 +0100

    Update main.py

--- examples/main.py
+++ examples/main.py
@@ -1,19 +1,11 @@
 from reduced_state_caldeira_leggett.plot import plot_isotropic_noise_kernel
 from reduced_state_caldeira_leggett.system import (
-
     HYDROGEN_NICKEL_SYSTEM,
-
-
-
     SimulationConfig,
 )
 
 if __name__ == "__main__":
-
     system = HYDROGEN_NICKEL_SYSTEM
-
-
-
     config = SimulationConfig(
         shape=(1, 1),
         resolution=(31, 31),
@@ -21,10 +13,4 @@ if __name__ == "__main__":
         type="bloch",
         temperature=150,
     )
-
-    # plot_new_noise_operators(system, config, n=1)
     plot_isotropic_noise_kernel(system, config)
-
-    # plot_basis_states(system, config)
-    # plot_state_against_t(system, config, n=1000, step=500)
-    # plot_initial_state(system, config)
